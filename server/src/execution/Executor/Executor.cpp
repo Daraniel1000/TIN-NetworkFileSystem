ad367828b0296ff8d38edd9811ed1ef461272bde
Maciej <daraniel1000@gmail.com>
2020-05-12 22:29:57 +0200

    added Executor stuff

---
+++ server/src/execution/Executor/Executor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Waluigi on 12.05.2020.
+//
+
+#include "Executor.h"
---
+++ server/src/execution/Executor/Executor.h
@@ -0,0 +1,14 @@
+//
+// Created by Waluigi on 12.05.2020.
+//
+
+#ifndef MYNFS_EXECUTOR_H
+#define MYNFS_EXECUTOR_H
+
+
+class Executor {
+
+};
+
+
+#endif //MYNFS_EXECUTOR_H
