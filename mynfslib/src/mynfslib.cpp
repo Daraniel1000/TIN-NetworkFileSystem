1823899662ae333591b26442c9304373ea1cd2f8
wpaszko <293114@pw.edu.pl>
2020-05-23 22:14:52 +0200

    Mynfs rest of funcs added

--- mynfslib/src/mynfslib.cpp
+++ mynfslib/src/mynfslib.cpp
@@ -10,6 +10,8 @@
 #include <application/mynfs/requests/CloseRequest.h>
 #include <application/mynfs/replies/UnlinkReply.h>
 #include <application/mynfs/requests/UnlinkRequest.h>
+#include <mynfslib.h>
+
 #include "mynfslib.h"
 #include "endpoint/ClientEndpoint.h"
 
@@ -33,13 +35,92 @@ int16_t mynfs_open(char const *host, char const *path, uint8_t oflag)
 
 int16_t mynfs_read(char const *host, int16_t fd, void *buf, int16_t count)
 {
+    // create endpoint
     ClientEndpoint clientEndpoint;
 
+    // send request and get reply
     ReadReply readReply = clientEndpoint.send<ReadRequest, ReadReply>
             (
                     NetworkAddress(host),
                     ReadRequest(fd, count)
             );
 
+    // react to error here
+    readReply.getError();
+
+    //return
     return readReply.getData().getSize();
+}
+
+int16_t mynfs_write(char *host, int16_t fd, void *buf, int16_t count)
+{
+    // create endpoint
+    ClientEndpoint clientEndpoint;
+
+    // send request and get reply
+    WriteReply readReply = clientEndpoint.send<WriteRequest, WriteReply>
+            (
+                    NetworkAddress(host),
+                    WriteRequest(fd, buf,count)
+            );
+
+    // react to error here
+    readReply.getError();
+
+    return readReply.getCount();
+}
+
+int32_t mynfs_lseek(char *host, int16_t fd, int32_t offset, uint8_t whence)
+{
+    // create endpoint
+    ClientEndpoint clientEndpoint;
+
+    // send request and get reply
+    LseekReply readReply = clientEndpoint.send<LseekRequest, LseekReply>
+            (
+                    NetworkAddress(host),
+                    LseekRequest(fd, offset, whence)
+            );
+
+    // react to error here
+    readReply.getError();
+
+    //return offset
+    return readReply.getOffset();
+}
+
+int8_t mynfs_close(char *host, int16_t fd)
+{
+    // create endpoint
+    ClientEndpoint clientEndpoint;
+
+    // send request and get reply
+    CloseReply readReply = clientEndpoint.send<CloseRequest, CloseReply>
+            (
+                    NetworkAddress(host),
+                    CloseRequest(fd)
+            );
+
+    // react to error here
+    readReply.getError();
+
+   return 0;
+}
+
+int8_t mynfs_unlink(char *host, char *path)
+{
+    // create endpoint
+    ClientEndpoint clientEndpoint;
+
+    // send request and get reply
+    UnlinkReply readReply = clientEndpoint.send<UnlinkRequest, UnlinkReply>
+            (
+                    NetworkAddress(host),
+                    UnlinkRequest(path)
+            );
+
+    // react to error here
+    readReply.getError();
+
+    return 0;
 }
